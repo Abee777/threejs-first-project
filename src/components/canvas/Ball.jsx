2d6e4c2180dd15f1225062f8959aeb70629acaf8
Vladimir Salatic <vrsalatic@gmail.com>
2023-07-21 18:43:39 +0200

    Tech component done

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -5,13 +5,26 @@ import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/d
 import CanvasLoader from '../Loader'
 
 const Ball = (props) => {
+  // Texture for balls
   const [decal] = useTexture([props.imgUrl]);
   return (
     <Float speed={1.75} rotationIntesity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow>
-
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[ 2 * Math.PI, 0, 6.25]} // to mirror the icons
+          flatShading
+        />
       </mesh>
     </Float>
   )
