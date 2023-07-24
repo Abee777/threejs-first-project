6d1016e34af821d817bcab55095b1f393d9f3b6c
Vladimir Salatic <vrsalatic@gmail.com>
2023-07-24 23:35:41 +0200

    Feedbacks - more @media styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,7 @@ module.exports = {
       },
       screens: {
         xs: "450px",
+        mdl: "941px"
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
